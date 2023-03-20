8f18b4704d2f67ff9b677b70a0b9efaed6367489
diyon335 <diyonreal@gmail.com>
2023-03-20 21:07:12 +0100

    fixed viewer. now shows actual simulation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import time
 import mujoco
-from mujoco import viewer
+import mujoco_viewer
 
 # Give the path to the model's scene.xml file
 quadruped = "./models/unitree_a1/scene.xml"
@@ -9,26 +9,29 @@ manipulator = "./models/franka_emika_panda/scene.xml"
 # Choose which model you want to load
 robot_model = quadruped
 
+# Time in seconds
+simulation_time = 15
+
 
 def main():
     model = mujoco.MjModel.from_xml_path(robot_model)
-
     data = mujoco.MjData(model)
 
-    renderer = mujoco.Renderer(model)
-
-    context = mujoco.GLContext(900, 800)
-    context.make_current()
+    viewer = mujoco_viewer.MujocoViewer(model, data)
 
     start = time.time()
-    while time.time() - start < 5:
+    while time.time() - start < simulation_time:
 
-        data.ctrl[0] += 1.0
-        mujoco.mj_step(model, data)
+        if viewer.is_alive:
+            data.ctrl[0] += 1.0
+            mujoco.mj_step(model, data)
 
-        renderer.update_scene(data)
+            viewer.render()
 
-    renderer.render()
+        else:
+            break
+
+    viewer.close()
 
 
 if __name__ == '__main__':
