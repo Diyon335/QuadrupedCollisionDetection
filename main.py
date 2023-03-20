e1fd5450bea8c38758fc546e645ae2b33eba8192
diyon335 <diyonreal@gmail.com>
2023-03-20 17:18:53 +0100

    added viewer

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
+import time
 import mujoco
 from mujoco import viewer
 
+# Give the path to the model's scene.xml file
+quadruped = "./models/unitree_a1/scene.xml"
+manipulator = "./models/franka_emika_panda/scene.xml"
+
+# Choose which model you want to load
+robot_model = quadruped
+
 
 def main():
-    model = mujoco.MjModel.from_xml_path("./models/unitree_a1/scene.xml")
+    model = mujoco.MjModel.from_xml_path(robot_model)
+
     data = mujoco.MjData(model)
-    viewer.launch(model, data)
+
+    renderer = mujoco.Renderer(model)
+
+    context = mujoco.GLContext(900, 800)
+    context.make_current()
+
+    start = time.time()
+    while time.time() - start < 5:
+
+        data.ctrl[0] += 1.0
+        mujoco.mj_step(model, data)
+
+        renderer.update_scene(data)
+
+    renderer.render()
 
 
 if __name__ == '__main__':
